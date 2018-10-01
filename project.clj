31ab6fa60ee7b2e0d9a1681328a27869923b9655
Kevin Ndung'u <kevgathuku@gmail.com>
2018-10-01 11:33:05 +0300

    Add code from lecture 1a

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject intro "0.0.1-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :aot [intro.core]
+  :main intro.core)
---
+++ src/intro/core.clj
@@ -0,0 +1,46 @@
+;; Lec 1a
+
+(ns intro.core
+  (:gen-class))
+
+(defn square
+  [x]
+  (* x x))
+
+(defn average
+  [x y]
+  (/ (+ x y) 2))
+
+(defn mean-square
+  [x y]
+  (average (square x) (square y)))
+
+(defn abs
+  [x]
+  (cond
+    (< x 0) (- x)
+    :else x))
+
+(defn improve
+  [guess x]
+  (average guess (/ x guess)))
+
+(defn good-enough?
+  [guess x]
+  (< (abs (- (square guess) x)) 0.001))
+
+
+(defn sqrt-guesser
+  [guess x]
+  (if (good-enough? guess x)
+    guess
+    (sqrt-guesser (improve guess x) x)))
+
+
+;; Start with a guess of 1
+(defn sqrt
+  [x]
+  (sqrt-guesser 1.0 x))
+
+(defn -main []
+  (println (sqrt 36)))
