07310ff0fecce4c304fe56150b52db7e2265198e
Kevin Ndung'u <kevgathuku@gmail.com>
2018-11-21 16:04:53 +0300

    Lec 2a: Add Newton's square root method

--- src/intro/two-a.clj
+++ src/intro/two-a.clj
@@ -60,16 +60,6 @@
   [a b]
   (sum square a inc b))
 
-
-;; Heron's square root method 
-;; Find a fixed point (sqrt) by calling a function repeatedly
-;; avg of y & (x / y)
-(defn sqrt
-  [x]
-  (fixed-point 
-    (fn [y] (average (/ x y) y))
-    1))
-
 ;; let -> define local variables
 ;; letfn -> define local functions supporting recursion
 (defn fixed-point
@@ -83,13 +73,14 @@
                    new
                    (iter new (f new))))]
     iter start (f start))))
-  
-;; Simplification of above sqrt method
-;; average-damp generates the fn passed to fixed-point
-(defn sqrt-damp
+
+;; Heron's square root method 
+;; Find a fixed point (sqrt) by calling a function repeatedly
+;; avg of y & (x / y)
+(defn sqrt
   [x]
-  (fixed-point
-    average-damp (fn [y] (/ x y))
+  (fixed-point 
+    (fn [y] (average (/ x y) y))
     1))
 
 ;; avg-damp -> returns fn that computes average of 2 oscillatting values
@@ -100,8 +91,45 @@
   (fn [x] (average (f x) x)))
 
 
+;; Simplification of above sqrt method
+;; average-damp generates the fn passed to fixed-point
+(defn sqrt-damp
+  [x]
+  (fixed-point
+    average-damp (fn [y] (/ x y))
+    1))
+
 ;; tests
 (defn sum-int-checker
   []
   (assert (= 6 (sum-int 0 3)))
   (assert (= 6 (sum-int-hof 0 3))))
+
+
+;; Newton's method for finding sqrt
+
+(def dx 0.00001)
+
+(defn deriv [f]
+  (fn [x] 
+    (/ (- (f (+ x dx))
+          (f x))
+       dx)))
+
+;; Newton's formula for finding f(x) = 0
+(defn newton-transform [f]
+  (fn [x] 
+    (- x (/ (f x) ((deriv f) x)))))
+
+(defn newtons-method
+  [f guess]
+  (fixed-point (newton-transform f) guess))
+
+
+(defn sqrt-newton
+  [x]
+  (newtons-method
+    (fn [y] (- (square y) x))
+    1.0))
+
+
